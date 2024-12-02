faa79c92c669931e656389e95fb02365a137f00f
Alberti Ádám <albertiadam2004@gmail.com>
2024-12-02 16:27:18 +0100

    more sorting

---
+++ run_webscrape.py
@@ -0,0 +1,23 @@
+from library import *
+from utils import Webscrape
+
+######### PARAMETERS #########
+url = "https://www.emag.hu/gyerekhazak-es-satrak/c?ref=subcat_1_fashion-grid_1"
+
+######## MAIN FUNC ##########
+
+def main():
+    wc = Webscrape()    
+    driver = wc.initialize_webdriver(url)
+    big = wc.get_data()
+    df, aggr_df = wc.create_df(big)
+    df
+    aggr_df
+    
+######## RUN ################
+main()
+
+
+
+
+
---
+++ utils.py
@@ -0,0 +1,37 @@
+from library import *
+class Webscrape:
+    def __init__(self):
+        """Init func"""
+    def initialize_webdriver(self,url:str) -> webdriver:
+        page_url = "https://www.emag.hu/gyerekhazak-es-satrak/c?ref=subcat_1_fashion-grid_1"
+        driver = webdriver.Chrome()
+        driver.get(page_url)
+        return driver
+
+    def get_data(self) -> list:
+        big = []
+        for x in range(100):
+            try:
+                fasz = driver.find_elements(By.XPATH, f'//*[@id="card_grid"]/div[{x}]/div/div/div[3]/div/h2/a')
+                asd = driver.find_elements(By.XPATH, f'//*[@id="card_grid"]/div[{x}]/div/div/div[4]/div[1]/p[2]')
+                zsar = driver.find_elements(By.XPATH, f'//*[@id="card_grid"]/div[{x}]/div/div/div[3]/div/div[1]/a/div[2]/span[1]')
+                print(fasz[0].text, asd[0].text, zsar[0].text)
+                lista = [fasz[0].text,asd[0].text,zsar[0].text]
+                big.append(lista)
+            except:
+                print("")
+        return big
+
+    def create_df(self,data_list:list) -> tuple:
+        tvk = pd.DataFrame(data_list)
+        tvk = tvk.rename(columns={0:'Név',1:'Ár',2:'Értékelés'})
+        tvk['Ár'] = tvk['Ár'].apply(lambda x: int(x[:-3].replace(".","")))
+        tvk = tvk.sort_values(by=['Ár','Értékelés'])
+
+        asd = tvk['Ár'].aggregate({
+            'Átlag' : 'mean',
+            'Maximum' : 'max',
+            'Minimum' : 'min'
+        })
+    
+        return tvk,asd   
